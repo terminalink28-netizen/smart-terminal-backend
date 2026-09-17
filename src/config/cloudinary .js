2e4c344432555b31b0d5519a3725758622412516
terminalink28-netizen <terminalink28@gmail.com>
2026-09-17 22:04:45 +0800

    Update cloudinary .js

--- src/config/cloudinary .js
+++ src/config/cloudinary .js
@@ -1,10 +1,10 @@
-import { v2 as cloudinary } from 'cloudinary';
+import cloudinary from 'cloudinary';
 import 'dotenv/config';
 
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinary;
+export default cloudinary.v2;
