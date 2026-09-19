9f82ed7175632d166d4edd97722a61371fade95e
terminalink28-netizen <terminalink28@gmail.com>
2026-09-19 11:25:09 +0800

    Update driver.routes.js

--- src/routes/driver.routes.js
+++ src/routes/driver.routes.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { registerDriver, getAvailableVans } from '../controllers/driver.controller.js';
+import { registerDriver } from '../controllers/driver.controller.js';
 import { handleLicenseUpload } from '../middleware/upload.middleware.js';
 
 const router = express.Router();
 
-router.get('/available-vans', getAvailableVans);
 router.post('/register', handleLicenseUpload, registerDriver);
 
 export default router;
