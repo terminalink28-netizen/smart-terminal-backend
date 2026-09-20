a8910c86dba559db2c654b0cfba7971949a368dc
terminalink28-netizen <terminalink28@gmail.com>
2026-09-20 17:14:34 +0800

    Update trip.controller.js

--- src/controllers/trip.controller.js
+++ src/controllers/trip.controller.js
@@ -30,7 +30,7 @@ const VALID_TRANSITIONS = {
 const ACTIVE_STATUSES = ['BOARDING', 'DEPARTING', 'DEPARTED', 'ARRIVING', 'DELAYED'];
 
 const TRIP_INCLUDE = {
-  driver: { select: { id: true, name: true } },
+  driver: { select: { id: true, name: true, contactNumber: true } },
   route:  true,
   van:    true,
 };
