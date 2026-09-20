605374c5ad5f54300bbe0c329641c7bd7c77318d
terminalink28-netizen <terminalink28@gmail.com>
2026-09-20 11:33:33 +0800

    Update trip.routes.js

--- src/routes/trip.routes.js
+++ src/routes/trip.routes.js
@@ -1,7 +1,7 @@
 // src/routes/trip.routes.js
 import express from 'express';
 import * as tripController from '../controllers/trip.controller.js';
-import { requireAuth, requireRole } from '../middleware/auth.js';
+import { requireAuth, requireRoles } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
